bd8d52fc3d488e750a8024f8091cec7996fba6e3
kokihoon <rhrlgns777@gmail.com>
2020-07-10 01:52:25 +0900

    비로그인 시 팀원찾기 수정

--- src/pages/PageLayout.tsx
+++ src/pages/PageLayout.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { CreateButton } from '../components/SideBar/style';
+import { useSelector, useDispatch } from 'react-redux';
+import ModalComponent from 'components/Modal/index';
+import Login from 'components/Login/index';
+import Register from 'components/Register/index';
+import 'antd/dist/antd.css';
+import { reset } from 'store/auth/action';
 
 const MainLayout = styled.div`
   padding: 7.5rem 10% 0;
@@ -12,6 +18,23 @@ const PageTitle = styled.h1`
 `;
 
 const PageLayout: React.FC = ({ children }) => {
+  const [visible, setVisible] = useState(false);
+  const { isLoggedIn } = useSelector((state: StoreState) => state.auth.me);
+  const { status } = useSelector((state: StoreState) => state.auth.emailCheck);
+
+  const dispatch = useDispatch();
+
+  const handleVisible = (): void => {
+    setVisible(true);
+  };
+
+  const handleCancel = useCallback(async () => {
+    setVisible(false);
+    dispatch(reset());
+
+    return visible;
+  }, [visible]);
+
   return (
     <MainLayout>
       <div
@@ -34,11 +57,24 @@ const PageLayout: React.FC = ({ children }) => {
           지금바로 FIT이 맞는 사람들을 찾아보세요!
         </div>
         <div style={{ display: 'flex' }}>
-          <Link to="/team/create">
-            <CreateButton>팀원 찾기</CreateButton>
-          </Link>
+          {isLoggedIn ? (
+            <Link to="/team/create">
+              <CreateButton>팀원 찾기</CreateButton>
+            </Link>
+          ) : (
+            <CreateButton onClick={handleVisible}>팀원 찾기</CreateButton>
+          )}
         </div>
       </div>
+      {visible && (
+        <ModalComponent
+          handleCancel={handleCancel}
+          visible={visible}
+          width={700}
+        >
+          {status == 'register' ? <Register /> : <Login />}
+        </ModalComponent>
+      )}
       <PageTitle>팀 리스트</PageTitle>
       {children}
     </MainLayout>
